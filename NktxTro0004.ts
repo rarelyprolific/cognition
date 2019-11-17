967bdac9f0f8ec91d05acb82de20f7d85019f44a
Alan Gryniewicz <alan.gryniewicz@gmail.com>
2019-11-17 16:43:51 +0000

    Work in progress pattern display rendering

--- NktxTro0004.ts
+++ NktxTro0004.ts
@@ -3,6 +3,10 @@ class NktxTro0004 {
   private textPrinter: Cognition.ICognitionEffect;
   private module: Cognition.Audio.ProTrackerModule;
   private moduleInfo: number = 0;
+  private currentChannel0PatternEntry: Cognition.Audio.ProTrackerPatternEntry;
+  private currentChannel1PatternEntry: Cognition.Audio.ProTrackerPatternEntry;
+  private currentChannel2PatternEntry: Cognition.Audio.ProTrackerPatternEntry;
+  private currentChannel3PatternEntry: Cognition.Audio.ProTrackerPatternEntry;
 
   public run = () => {
     // Set up the textprinter
@@ -11,7 +15,7 @@ class NktxTro0004 {
 
     // Set up the modplayer and load the module
     const modplayer = new Protracker();
-    const encodedModule = window.atob(Lite13Module);
+    const encodedModule = window.atob(PostureSliding);
 
     // Decode the module back from base64 into a byte array
     const moduleLength = encodedModule.length;
@@ -36,9 +40,17 @@ class NktxTro0004 {
       //     output[i] = Math.random();
       //   }
       this.moduleInfo++;
+      this.currentChannel0PatternEntry = this.module.Patterns[0].Channels[0].PatternEntries[3];
+      this.currentChannel1PatternEntry = this.module.Patterns[0].Channels[1].PatternEntries[3];
+      this.currentChannel2PatternEntry = this.module.Patterns[0].Channels[2].PatternEntries[3];
+      this.currentChannel3PatternEntry = this.module.Patterns[0].Channels[3].PatternEntries[3];
     };
 
     moduleReplayerAudioNode.connect(audioContext.destination);
+
+    // TODO: I need to figure out the period table logic and how to convert
+    // period values to their equivalent "notes".
+    console.log(this.module);
 
     this.loop();
   };
@@ -55,6 +67,22 @@ class NktxTro0004 {
 
     this.textPrinter.draw("MODULENAME: " + this.module.SongName.toUpperCase(), 16, 16);
     this.textPrinter.draw("TIME: " + this.moduleInfo, 16, 48);
+
+    if (this.currentChannel0PatternEntry !== undefined) {
+      this.textPrinter.draw("CH0: " + this.currentChannel0PatternEntry.Note, 16, 80);
+    }
+
+    if (this.currentChannel1PatternEntry !== undefined) {
+      this.textPrinter.draw("CH1: " + this.currentChannel1PatternEntry.Note, 200, 80);
+    }
+
+    if (this.currentChannel2PatternEntry !== undefined) {
+      this.textPrinter.draw("CH2: " + this.currentChannel2PatternEntry.Note, 400, 80);
+    }
+
+    if (this.currentChannel3PatternEntry !== undefined) {
+      this.textPrinter.draw("CH3: " + this.currentChannel3PatternEntry.Note, 600, 80);
+    }
 
     // Run the text printer
     this.textPrinter.draw("NEOKORTEX", 642, 550);
